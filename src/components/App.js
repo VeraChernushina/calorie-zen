a93aef275ef87dfd20154981c0282ff1ee547a56
Vera Chernushina <vera.chernushina@yandex.ru>
2021-12-21 18:14:24 +0300

    feat: make components AppContainer and Header

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import AppContainer from './AppContainer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppContainer />
+    </>
+  );
+}
+
+export default App;
