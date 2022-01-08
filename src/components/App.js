339cf016e4f5ab15f8ef90fc9eddeeb79a9a021f
Vera Chernushina <vera.chernushina@yandex.ru>
2022-01-08 16:17:36 +0300

    feat: react-router-dom, BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import Header from './Header';
 import AppContainer from './AppContainer';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <AppContainer />
-    </>
+    </BrowserRouter>
   );
 }
 
